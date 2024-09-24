ee40e7cd222b8ef409f9b74a51be504ffb9f8a4b
Prahallad Padhan <148340970+Prahallad-Padhan@users.noreply.github.com>
2024-09-24 13:54:03 +0530

    Add files via upload

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './Header/Header';
+import GPA from './GPA/GPA';
+import Footer from './Footer/Footer';
 import './Calculator.css';
 
 const Calculator = () => {
   return (
     <div className='page5'>
       <Header/>
-      Calculator
+      <GPA />
+      <Footer />
     </div>
   )
 }
