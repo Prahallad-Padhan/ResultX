8dc1dfc12a5137df0ce7c03316a64e86548d86af
Prahallad Padhan <148340970+Prahallad-Padhan@users.noreply.github.com>
2024-09-25 21:57:28 +0530

    Update Result.js

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './Header/Header';
+import MainTable from './Table/MainTable';
+import Footer from './Footer/Footer';
 import './Result.css';
 
 const Result = () => {
   return (
     <div className='page2'>
       <Header/>
-      Result
+      <MainTable />
+      <Footer/>
     </div>
   )
 }
